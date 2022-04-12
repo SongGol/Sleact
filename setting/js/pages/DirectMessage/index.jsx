2a103a1dc1828f63403f1bd770f5b56ea7bd3aa7
SongGol <dldmsgus2@gmail.com>
2022-04-12 20:09:43 +0900

    :lipstick: chatbox :bug: 화면에 표시 안되는 버그

--- setting/js/pages/DirectMessage/index.jsx
+++ setting/js/pages/DirectMessage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Header } from '@pages/DirectMessage/styles';
 import fetcher from '@utils/fetcher';
+import ChatBox from '@components/ChatBox';
 import useSWR from 'swr';
 import gravatar from 'gravatar';
 import { useParams } from 'react-router-dom';
@@ -20,8 +21,8 @@ const DirectMessage = () => {
                 <img src={gravatar.url(userData.email, {s: "24px", d: 'retro'})} alt={userData.nickname}/>
                 <span>{userData.nickname}</span>
             </Header>
-            {/* <ChatList />
-            <ChatBox /> */}
+            {/* <ChatList /> */}
+            <ChatBox chat=""/>
         </Container>
     )
 };
