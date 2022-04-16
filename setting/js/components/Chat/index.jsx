624965727a4fd783b9eb9b409919a744d7f4e426
SongGol <dldmsgus2@gmail.com>
2022-04-16 16:58:27 +0900

    :sparkles: 정규표현식 적용

--- setting/js/components/Chat/index.jsx
+++ setting/js/components/Chat/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import gravatar from 'gravatar';
 import { ChatWrapper } from '@components/Chat/styles';
+import regexifyString from 'regexify-string';
 import dayjs from 'dayjs';
+import { Link, useParams } from 'react-router-dom';
 
 const Chat = ({ key, data }) => {
     const user = data.Sender;
-    console.log(`chat component user: ${user}`)
+    const { workspace } = useParams();
+
+    const result = useMemo(() => regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)\]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+            const arr = match.match(/@\[(.+?)\]\((\d+?)\)/);
+            if (arr) {
+                return (
+                    <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`} >
+                        @{arr[1]}
+                    </Link>
+                );
+            }
+            return <br key={index} />
+        }
+    }), [data.content]);
 
     return (
         <ChatWrapper>
@@ -17,10 +35,10 @@ const Chat = ({ key, data }) => {
                     <b>{user.nickname}</b>
                     <span>{dayjs(data.createdAt)}</span>
                 </div>
-                <p>{data.content}</p>
+                <p>{result}</p>
             </div>
         </ChatWrapper>
     )
 };
 
-export default Chat;
+export default memo(Chat);
